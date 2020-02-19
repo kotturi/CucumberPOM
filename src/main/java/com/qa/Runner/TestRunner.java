f76f7b677106125c23922ce850bb3fbebe5b629d
Kotturi <Jyothi.Kotturi@parexel.com>
2020-02-19 10:33:01 +0530

    Report File

--- src/main/java/com/qa/Runner/TestRunner.java
+++ src/main/java/com/qa/Runner/TestRunner.java
@@ -14,21 +14,25 @@ import cucumber.api.testng.TestNGCucumberRunner;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "C:/Users/KotturJ/eclipse-workspace/CucumberPOM/src/main/java/com/qa/features", //the path of the feature files
-		glue={"com/qa/StepDefinations"}, //the path of the step definition files
+		glue={"com.qa.StepDefinations"}, //the path of the step definition files
 		//Testng report
-		format= {
+		/*format= {
 				"pretty",
 				"html:target/cucumber-reports/cucumber-html-report",
 				"json:target/cucumber-reports/TestReport.json",
 				"junit:target/cucumber-reports/cucumber.xml",
 				//"return: target/cucumber-reports/return.txt"
-		//		"return:target/cucumber-reports/return.txt"
+*/		//		"return:target/cucumber-reports/return.txt"
 //		format= {"pretty",
 //				"html:test-output", 
 //				"json:json_output/cucumber.json", 
 //				"junit:junit_xml/cucumber.xml",
-				}, //to generate different types of reporting
-		plugin="json:target/cucumber-reports/TestReport.json",
+		//		}, //to generate different types of reporting
+	//	plugin="json:target/cucumber-reports/TestReport.json",
+				plugin = {"pretty", 
+						  "html:target/cucumber-html-report",
+   					       "json:target/cucumber.json", 
+   					       "junit:target/cucumber.xml"},
 		monochrome = true, //display the console output in a proper readable format
 		strict = true, //it will check if any step is not defined in step definition file
 		dryRun = false //to check the mapping is proper between feature file and step def file
